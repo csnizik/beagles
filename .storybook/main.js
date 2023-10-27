91617bd653136ecbed6f9b62124d096724d66a98
csnizik <csnizik@tulane.edu>
2023-10-27 11:34:44 -0500

    sb configs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 /** @type { import('@storybook/server-webpack5').StorybookConfig } */
 const config = {
   stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(json|yaml|yml)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", '@lullabot/storybook-drupal-addon'],
   framework: {
     name: "@storybook/server-webpack5",
     options: {},
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,25 @@
 /** @type { import('@storybook/server').Preview } */
 const preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
     },
+    server: {
+      url: 'https://newpods.ddev.site',
+    },
   },
-};
+  globals: {
+    drupalTheme: 'fpacds',
+    supportedDrupalThemes: {
+      claro: { title: 'Claro' },
+      olivero: { title: 'Olivero' },
+      stark: { title: 'Stark' },
+    },
+  },
+}
 
 export default preview;
